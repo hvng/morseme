9ff4bdbf4265886b169df98501787e973538cc12
Hung <hvngcs@gmail.com>
2020-09-29 05:19:37 +0900

    Init project

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='morseme',
+    version='0.1',
+    description='A simple morse to human-readable-text converter',
+    license='MIT',
+    author='Hung Nguyen',
+    author_email='hvngcs@gmail.com',
+    url='https://github.com/hvng/morseme',
+    download_url='https://github.com/hvng/morseme/archive/v_01.tar.gz',
+    keywords=['MORSE', 'MORSEME', ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
