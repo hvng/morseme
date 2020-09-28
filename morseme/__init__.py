c80f68f98aa414b00e21ba60a9435bf57ce83b3a
Hung <hvngcs@gmail.com>
2020-09-29 08:29:12 +0900

    update structure

---
+++ morseme/__init__.py
@@ -0,0 +1,1 @@
+from .morseme import encrypt, decrypt
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='morseme',
-    py_modules=['morseme'],
+    packages = ['morseme'],
     version='0.1.1',
     description='A simple morse to human-readable-text converter',
     license='MIT',
